b9f6705bc47aaced75f62a795bbbe053dd98b263
carlosnaranjo193 <111545226+carlosnaranjo193@users.noreply.github.com>
2022-09-08 21:09:00 -0500

    Add files via upload

---
+++ Biblioteca/Biblioteca/gestionPrestamos/models.py
@@ -0,0 +1,28 @@
+import email
+from django.db import models
+
+# Create your models here.
+
+class libro(models.Model):
+    Isbn=models.CharField(max_length=25,primary_key=True)
+    titulo=models.TextField(max_length=100,unique=True,null=False)
+    editorial=models.CharField(max_length=50,null=False)
+    autor=models.CharField(max_length=50,null=False)
+    no_page=models.IntegerField(null=True)
+    
+class Estudiante(models.Model):
+    documento=models.IntegerField(primary_key=True)
+    nombre=models.TextField(max_length=50,null=False)
+    apellido=models.TextField(max_length=50,null=False)
+    email=models.EmailField(unique=True)
+    telefono=models.CharField(max_length=50)
+    
+class Prestamo(models.Model):
+    id=models.IntegerField(auto_created=True,primary_key=True)
+    estudiante=models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    libro=models.ForeignKey(libro, on_delete=models.CASCADE)
+    fecha=models.DateField(auto_now=True)
+    
+    
+    
+     
